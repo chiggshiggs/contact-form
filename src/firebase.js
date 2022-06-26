9ea1d7530d127e5836b2aaec4096cd6d9471c144
chiggshiggs <chirag.goel.mat21@itbhu.ac.in>
2022-06-26 17:53:47 +0530

    My commit

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcQr-xm0gqX0C0eB025v63feoRXRyx6dM",
+  authDomain: "team-expansion-54ae7.firebaseapp.com",
+  databaseURL: "https://team-expansion-54ae7-default-rtdb.firebaseio.com",
+  projectId: "team-expansion-54ae7",
+  storageBucket: "team-expansion-54ae7.appspot.com",
+  messagingSenderId: "932038990518",
+  appId: "1:932038990518:web:02f65a0122d0ff39bfa417",
+  measurementId: "G-NCB4XT3N2C"
+};
+
+// Use this to initialize the firebase App
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// Use these for db & auth
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth, db };
+
+
+
